035f378c445ecbad418330ae455ca945a7748a40
Eric Li <ericli@knights.ucf.edu>
2021-05-22 11:40:54 -0600

    Add event listener and query selectors for showMyPosterBtn

--- src/main.js
+++ src/main.js
@@ -5,18 +5,22 @@ var savedPosters = [];
 var currentPoster;
 
 // event listeners go here 👇
-window.onload = newPoster
+//window.onload = newPoster
 var posterImage = document.querySelector(".poster-img");
 var posterTitle = document.querySelector(".poster-title");
 var posterQuote = document.querySelector(".poster-quote");
 var randomImgBtn = document.querySelector(".show-random");
-var form = document.querySelector(".poster-form");
+var formPage = document.querySelector(".poster-form");
 var showFormBtn = document.querySelector(".show-form");
 var mainPage = document.querySelector(".main-poster");
 var goBackBtn = document.querySelector(".show-main");
 var showSavedBtn = document.querySelector(".show-saved");
 var showSaved = document.querySelector(".saved-posters");
-var backToMainBtn = document.querySelector(".back-to-main")
+var backToMainBtn = document.querySelector(".back-to-main");
+var showMyPosterBtn = document.querySelector(".make-poster");
+var posterImgInput = document.getElementById("poster-image-url");
+var posterTitleInput = document.getElementById("poster-title");
+var posterQuoteInput = document.getElementById("poster-quote");
 
 randomImgBtn.addEventListener("click", newPoster);
 showFormBtn.addEventListener("click", showForm);
@@ -24,17 +28,20 @@ goBackBtn.addEventListener("click", goBackToHomePage);
 // could possibly use hideShowMainPage in place of showForm and goBackToHomePage
 showSavedBtn.addEventListener("click", showSavedPosters);
 backToMainBtn.addEventListener("click", hideShowMainPage);
+showMyPosterBtn.addEventListener("click", showMyPoster);
 
 
 // functions and event handlers go here 👇
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
+
 function newPoster() {
   posterImage.src = images[getRandomIndex(images)];
   posterTitle.innerText = titles[getRandomIndex(titles)];
   posterQuote.innerText = quotes[getRandomIndex(quotes)];
 }
+
 function hideShowMainPage() {
   showSaved.classList.toggle("hidden");
   mainPage.classList.toggle("hidden");
@@ -42,7 +49,7 @@ function hideShowMainPage() {
 
 function showForm() {
   event.preventDefault();
-  hide(form, true);
+  hide(formPage, true);
   hide(mainPage, false);
 }
 
@@ -52,7 +59,7 @@ function showSavedPosters() {
 }
 
 function goBackToHomePage() {
-  hide(form, false);
+  hide(formPage, false);
   hide(mainPage, true);
 }
 // checking if element has a hidden class, you want to remove it, if it doesn't, you want to add it
